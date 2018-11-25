5abfadddacd92f1a1f6d9d4b2ca17af78effaf30
Matthew J Gunton <mjg422@lehigh.edu>
2018-11-25 18:40:08 -0500

    success! all 3 have been completed

--- nova/src/wallet/SendFunds/SendFunds.js
+++ nova/src/wallet/SendFunds/SendFunds.js
@@ -1,15 +1,58 @@
+//testing to see if this works rn, want to make sure all values are being successfully
+  //passed into the state
+
 import React, {Component} from 'react';
 
 export default class SendFunds extends Component{
+
+  state = {
+    whom: "",
+    value: "",
+    gas: ""
+  }
 
   send(){
     console.log("Did it transfer?");
   }
 
+  handleWhomChange = (e)=>{
+    this.setState({
+      whom: e.target.value
+    })
+  }
+
+  handleValueChange = (e)=>{
+    this.setState({
+      value: e.target.value
+    })
+  }
+
+  handleGasChange = (e)=>{
+    this.setState({
+      gas: e.target.value
+    })
+  }
+
+  handleSubmit = (e)=>{
+    e.preventDefault();
+    console.log(this.state);
+
+    this.props.send(this.state);
+  }
+
   render(){
     return(
-      <div>
-
+      <div className = "right">
+        <form onSubmit = {this.handleSubmit}>
+          <label>Send to Whom?</label><br/><input value = {this.state.whom} onChange = {this.handleWhomChange} /><br/>
+          <label>Value(ETH)</label><br/><input value = {this.state.value} onChange = {this.handleValueChange} type = "number"/><br/>
+          <label>Gas:</label><br/>
+            <input onChange = {this.handleGasChange} name = "gas" type = 'radio' value = "2000"/>Standard<br/>
+            <input onChange = {this.handleGasChange} name = "gas" type = 'radio' value = "4000"/>Fast<br/>
+            <input onChange = {this.handleGasChange} name = "gas" type = 'radio' value = "8000"/>Premium<br/>
+            <input onChange = {this.handleGasChange} name = "gas" type = 'radio' value = "10000"/>Lightning<br/>
+          <button>Send</button>
+        </form>
       </div>
     )
   }
--- nova/src/wallet/Wallet.js
+++ nova/src/wallet/Wallet.js
@@ -10,31 +10,35 @@ import Web3 from 'web3'
 export default class Wallet extends Component{
 
   state = {
-    balance: 13.90
+    connected: false,
+    balance: 13.90,
+    provider: {},
+    address: "",
+    sent: null
   }
 
-  constructor(props) {
-    super(props);
-
-
     //Task 1: connected to metamask!
-    this.web3 = new Web3(Web3.givenProvider);
-    if (typeof web3 != 'undefined') {
-      console.log('found metamask');
-      this.web3Provider = Web3.currentProvider
-    } else {
-      console.log("Metamask is not found");
-      this.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545')
+  login(){
+    if (typeof window.web3 !== 'undefined') {
+      console.log('check');
+      const provider = new Web3(window.web3.currentProvider);
+      provider.eth.getAccounts().then(addresses => {
+        this.setState({ connected: true, provider, address: addresses[0]})
+      }).catch(error => {
+        this.setState({ error })
+        console.log(error);
+        });
+
     }
 
   }
 
     //Task 2: got balance from metamask
     getBalance(){
+
       //for some reason this is inconsistent
         //sometimes address is invalid
-      var address = this.web3.eth.accounts._provider.selectedAddress;
-      this.web3.eth.getBalance(address)
+      this.state.provider.eth.getBalance(this.state.address)
       .then((result)=>{
         this.setState({
           balance: result
@@ -43,58 +47,74 @@ export default class Wallet extends Component{
     }
 
     //Task 3: send transactions
-    send = () => {
-      const keystore = "Contents of keystore file";
-      const decryptedAccount = this.web3.eth.accounts.decrypt(keystore, 'PASSWORD');
+      //the imbetweeners need to be handled outside
+    send = ({whom, value, gas}) => {
 
-      const rawTransaction = {
-        "from": "Keystore account id",
-        "to": "Account you want to transfer to",
-        "value": this.web3.utils.toHex(this.web3.utils.toWei("0.001", "ether")),
-        "gas": 2000,
-        "chainId": 3
-      };
+      // this.state.provider.eth.getAccounts().then(addresses => {
+      //   console.log(addresses);
+      // })
 
-      decryptedAccount.signTransaction(rawTransaction)
-      .then(signedTx => this.web3.eth.sendSignedTransaction(signedTx.rawTransaction))
-      .then(receipt => console.log("Transaction receipt: ", receipt))
-      .catch(err => console.error(err));
+      this.state.provider.eth.sendTransaction({
+        from: this.state.address,
+        to: whom,
+        value: value,
+        gas: gas
+      })
+        .then(addresses => {
+          this.setState({ sent: true})
+        }).catch(error => {
+          this.setState({ error, sent: false })
+          console.log(error);
+          });
 
     }
 
 
   render(){
-    return(
-      <div className = "walletBody">
-        <Balance value = {this.state.balance} />
+
+    const display = !this.state.connected ? (
+          <div>
+            <button onClick = {() => this.login()}>Login with MetaMask</button>
+          </div>
+      ) : (
+        <div>
+          <h1>Locked and Loaded, sir</h1>
+        </div>
+      );
+      const btnOrNah = !this.state.connected? (
+        <button className = "btn">Login</button>
+      ) : (
         <button className = "btn" onClick = { () => this.getBalance()}>Get Balance</button>
-        <Split side = "horizontal"/>
-        <Split side = "horizontal" double = 'double'/>
+      );
+    return(
+      <div>
 
-        <div className = "card">
+        {display}
+
+        <div className = "walletBody">
+          <Balance value = {this.state.balance} />
+
+          {btnOrNah}
+          <Split side = "horizontal"/>
+          <Split side = "horizontal" double = 'double'/>
+
+          <div className = "card">
+          </div>
+
+          <div className = "card2">
+          </div>
+
+          <div className = "card3">
+          </div>
+
+          <div className = "card4">
+          </div>
+
+          <SendFunds send = {this.send}/>
+
+
+
         </div>
-
-        <div className = "card2">
-        </div>
-
-        <div className = "card3">
-        </div>
-
-        <div className = "card4">
-        </div>
-
-        {/*<SendFunds web3 = {this.web3} />*/}
-
-        <div className = "right">
-          <label>Send to Whom?</label><br/><input /><br/>
-          <label>Value(ETH)</label><br/><input type = "number"/><br/>
-          <label>Gas:</label><br/>
-            <input name = "gas" type = 'radio' value = "2000" selected/>Standard<br/>
-            <input name = "gas" type = 'radio' value = "4000" selected/>Fast<br/>
-            <input name = "gas" type = 'radio' value = "8000" selected/>Premium<br/>
-            <input name = "gas" type = 'radio' value = "10000" selected/>Lightning<br/>
-        </div>
-
       </div>
     )
   }
